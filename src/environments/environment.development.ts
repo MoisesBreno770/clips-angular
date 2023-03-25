cb3b912a518293056e3b39e4179d38b9c9d8990d
Moises Carvalho <moisesbreno7@gmail.com>
2023-03-25 05:14:31 -0300

    first commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyA-CKv-nzCdloYHzE1leQlZvOsIq0I7jyE",
+        authDomain: "clips-44b83.firebaseapp.com",
+        projectId: "clips-44b83",
+        storageBucket: "clips-44b83.appspot.com",
+        messagingSenderId: "996644942054",
+        appId: "1:996644942054:web:cd7ee1d67f40a224f8cb27"
+    },
+};
